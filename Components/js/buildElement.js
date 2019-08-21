a22669b71e5c8e3bb0d5578ac6d3c076bfffbe60
BPitts8019 <bradleyj.pitts@gmail.com>
2019-08-21 16:03:48 -0700

    Added updates to function; need to figure out java docs info

--- Components/js/buildElement.js
+++ Components/js/buildElement.js
@@ -1,25 +1,29 @@
-/*
-   input object:
-   {
-      tagName: "",
-      content: any,
-      classList: [],
-      parent: Element,
-      attributes: [ [name, value] ]
-   }
-*/
-function buildElement (tagName, content = null, className = null, url = null) {
+/**
+ * Generic function for building elements within a component
+ * @param input (object) {
+ *    tagName: "",
+ *    content: any,
+ *    classList: [],
+ *    parent: Element,
+ *    attributes: [ [name, value] ]
+ * }
+ */
+function buildElement ({tagName, content, classList, parent, attributes}) {
    // create element
-   const tag = document.createElement(tagName);
+   const newElement = document.createElement(tagName);
 
    //add properties
-   if (content) tag.textContent = content;
-   if (className) tag.classList.add(className);
-   if (url && tag.src !== undefined) tag.src = url;
-   if (url && tag.href !== undefined) tag.href = url;
+   if (content) newElement.textContent = content;
+   if (classList) classList.forEach(className => {
+      newElement.classList.add(className);
+   });
+   if (attributes) attributes.forEach(([name, value]) => {
+      newElement.setAttribute(name, value);
+   });
 
    //append to parent
+   parent.appendChild(newElement);
 
    //return element
-   return tag;
+   return newElement;
 }
